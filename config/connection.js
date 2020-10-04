e096ae782df58f00ee05e944213ea6349a924a9d
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-10-03 20:43:08 -0700

    Updated the connection to match heroku

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: 'sm9j2j5q6c8bpgyq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'burgersDB'
+    user: 'rmtmoa951zo9txob',
+    password: 's6bd7yrgdsyjp45n',
+    database: 'yxnzwsrld2i4h7z3'
 });
 
 connection.connect((err, res) => {
